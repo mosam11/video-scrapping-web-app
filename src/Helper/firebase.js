fe39d4c7c630db105002ea7b706837dd72719c64
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2019-04-24 23:35:51 -0700

    adding firebase to the project

---
+++ src/Helper/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/database"; // If using Firebase database
+import "firebase/storage"; // If using Firebase storage
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAzBqxa0XdBtAUh54VYQq3zTFErvv1K--E",
+  authDomain: "video-application-304df.firebaseapp.com",
+  databaseURL: "https://video-application-304df.firebaseio.com",
+  projectId: "video-application-304df",
+  storageBucket: "video-application-304df.appspot.com",
+  messagingSenderId: "710179737540"
+};
+firebase.initializeApp(config);
+
+const database = firebase.database();
+const storage = firebase.storage();
+
+export { firebase, storage, database }; //exporting database
--- src/components/userForm/userForm.jsx
+++ src/components/userForm/userForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { database } from "../../Helper/firebase";
 
 class UserForm extends Component {
   componentDidMount() {
