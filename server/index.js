04a812a6c7093d34750720b6c0244d15d948467d
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2019-05-05 15:08:28 +0500

    adding youtube search Api

--- server/index.js
+++ server/index.js
@@ -1,7 +1,49 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
+
+var bodyParser = require("body-parser");
+const cors = require("cors");
+
+// parse application/x-www-form-urlencoded
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+// parse application/json
+app.use(bodyParser.json());
+app.use(cors());
+
+var { google } = require("googleapis");
+
+var YOUTUBE_API_KEY = "AIzaSyBXv3zWJL803UPzgelx1UeCCpu_u5JknM0";
+
+var youtube = google.youtube({
+  version: "v3",
+  auth: YOUTUBE_API_KEY
+});
 
 app.get("/", (req, res) => res.send("Hello World!"));
+app.post("/videos", (req, res) => {
+  //Get videos from youtube
+  youtube.search.list(
+    {
+      part: "snippet",
+      q: "cats",
+      maxResults: 10
+    },
+    function(err, data) {
+      if (err) {
+        console.error("Error: " + err);
+      }
+      if (data) {
+        res.json(data.data.items);
+      }
+    }
+  );
+  //get videos from
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
