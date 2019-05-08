d834613e0b2c98c0c519f880528dd36170014088
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2019-05-08 15:05:44 +0500

    display fav videos in userInfo complete

--- src/components/userInfo/userInfo.jsx
+++ src/components/userInfo/userInfo.jsx
@@ -3,10 +3,109 @@ import { connect } from "react-redux";
 
 class UserInfo extends Component {
   render() {
+    const videos = [
+      {
+        thumb:
+          "https://media.istockphoto.com/photos/gentoo-penguin-waddling-along-on-a-white-sand-beach-picture-id511366776?k=6&m=511366776&s=612x612&w=0&h=FiyKvGeHCGbVLce6bkW7P7SzgC57V18jUcj0bs9Nv4w=",
+        title: "Video Title",
+        details:
+          "LoremLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  ipsum"
+      },
+      {
+        thumb:
+          "https://media.istockphoto.com/photos/gentoo-penguin-waddling-along-on-a-white-sand-beach-picture-id511366776?k=6&m=511366776&s=612x612&w=0&h=FiyKvGeHCGbVLce6bkW7P7SzgC57V18jUcj0bs9Nv4w=",
+        title: "Video Title",
+        details:
+          "LoremLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  ipsum"
+      },
+      {
+        thumb:
+          "https://media.istockphoto.com/photos/gentoo-penguin-waddling-along-on-a-white-sand-beach-picture-id511366776?k=6&m=511366776&s=612x612&w=0&h=FiyKvGeHCGbVLce6bkW7P7SzgC57V18jUcj0bs9Nv4w=",
+        title: "Video Title",
+        details:
+          "LoremLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  ipsum"
+      },
+      {
+        thumb:
+          "https://media.istockphoto.com/photos/gentoo-penguin-waddling-along-on-a-white-sand-beach-picture-id511366776?k=6&m=511366776&s=612x612&w=0&h=FiyKvGeHCGbVLce6bkW7P7SzgC57V18jUcj0bs9Nv4w=",
+        title: "Video Title",
+        details:
+          "LoremLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  ipsum"
+      },
+      {
+        thumb:
+          "https://media.istockphoto.com/photos/gentoo-penguin-waddling-along-on-a-white-sand-beach-picture-id511366776?k=6&m=511366776&s=612x612&w=0&h=FiyKvGeHCGbVLce6bkW7P7SzgC57V18jUcj0bs9Nv4w=",
+        title: "Video Title",
+        details:
+          "LoremLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  ipsum"
+      },
+      {
+        thumb:
+          "https://media.istockphoto.com/photos/gentoo-penguin-waddling-along-on-a-white-sand-beach-picture-id511366776?k=6&m=511366776&s=612x612&w=0&h=FiyKvGeHCGbVLce6bkW7P7SzgC57V18jUcj0bs9Nv4w=",
+        title: "Video Title",
+        details:
+          "LoremLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  ipsum"
+      },
+      {
+        thumb:
+          "https://media.istockphoto.com/photos/gentoo-penguin-waddling-along-on-a-white-sand-beach-picture-id511366776?k=6&m=511366776&s=612x612&w=0&h=FiyKvGeHCGbVLce6bkW7P7SzgC57V18jUcj0bs9Nv4w=",
+        title: "Video Title",
+        details:
+          "LoremLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  ipsum"
+      },
+      {
+        thumb:
+          "https://media.istockphoto.com/photos/gentoo-penguin-waddling-along-on-a-white-sand-beach-picture-id511366776?k=6&m=511366776&s=612x612&w=0&h=FiyKvGeHCGbVLce6bkW7P7SzgC57V18jUcj0bs9Nv4w=",
+        title: "Video Title",
+        details:
+          "LoremLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum  ipsum"
+      }
+    ];
     return (
-      <div>
-        <h1>Wellcome {this.props.user.userName}!</h1>
-        <h2>All Favourite Videos</h2>
+      <div id="userInfoMain">
+        <div id="mainDivTitle">
+          <h1>Wellcome {this.props.user.userName}!</h1>
+          <button
+            type="submit"
+            className="btn btn--primary"
+            id="editProfileBtn"
+          >
+            Edit Profile Info
+          </button>
+          <h2>All Favourite Videos</h2>
+        </div>
+        <div id="videoCardContainer">
+          {videos.length === 0 ? (
+            <span>No Video</span>
+          ) : (
+            videos.map((video, index) => {
+              return (
+                <div className="videoCard" key={index}>
+                  <img
+                    className="videoCardImg"
+                    src={video.thumb}
+                    alt={video.title}
+                  />
+                  <h2>{video.title}</h2>
+                  <p>{video.details}</p>
+                  <button
+                    type="submit"
+                    className="btn btn--primary playBtn videoCardBtn"
+                  >
+                    Play
+                  </button>
+                  
+                  <button
+                    type="submit"
+                    className="btn btn--primary delBtn"
+                  >
+                    Delete
+                  </button>
+                </div>
+              );
+            })
+          )}
+        </div>
       </div>
     );
   }
