0be7bcfe93614f01ba5252f55136f844ea7ac2a8
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2019-05-23 15:03:51 +0500

    Changing  text in nav based on auth

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import { connect } from "react-redux"; // To connect our component with the redux
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
+import { userSignOut } from "../../store/action";
+import { message } from "antd";
 
 class Navbar extends Component {
   render() {
@@ -13,16 +16,47 @@ class Navbar extends Component {
           <Link to="/" className="navItem">
             Home
           </Link>
-          <Link to="/userForm" className="navItem">
-            Login
-          </Link>
-          <Link to="/userInfo" className="navItem">
-            Profile
-          </Link>
+          {this.props.user === false ? (
+            <Link to="/userForm" className="navItem">
+              Login
+            </Link>
+          ) : (
+            <Link
+              to="/userForm"
+              onClick={() => {
+                this.props.userSignOut();
+                message.success("User SignOut");
+              }}
+              className="navItem"
+            >
+              Logout
+            </Link>
+          )}
+
+          {this.props.user !== false && (
+            <Link to="/userInfo" className="navItem">
+              {this.props.user.userName}
+            </Link>
+          )}
         </div>
       </div>
     );
   }
 }
 
-export default Navbar;
+//Object tomap state of redux to the props of the component
+
+const mapStateToProps = state => {
+  return {
+    user: state.userReducer.user
+  };
+};
+// Mapping out userSignIn with the dispatch and adding that in the props of the components
+const mapDispatchToProps = dispatch => ({
+  userSignOut: _ => dispatch(userSignOut()) // Mapping actions of redux to props
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Navbar);
